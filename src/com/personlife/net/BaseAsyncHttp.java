bee60e73e392dd508780a974ec8244eda93e3a95
feishuai <pfy@zju.edu.cn>
2015-08-06 16:59:10 +0800

    modify http

--- src/com/personlife/net/BaseAsyncHttp.java
+++ src/com/personlife/net/BaseAsyncHttp.java
@@ -9,34 +9,43 @@ import com.loopj.android.http.RequestParams;
 import com.personlife.utils.Utils;
 
 public class BaseAsyncHttp {
-	
-    public static final String HOST = "http://183.129.190.82:50001/v1";
-   
-//	public static final String HOST = "http://10.10.105.180:9000/v1";
-    private static AsyncHttpClient client = new AsyncHttpClient();
-    
-    public static void postReq(Context context,String host, String url, RequestParams params, JsonHttpResponseHandler hander) {
-    		client.post(host + url, params, hander);
-    }
-    public static void postReq(Context context,String url, RequestParams params, JsonHttpResponseHandler hander) {
-    	if(Utils.isNetworkAvailable(context))
-    		client.post(HOST + url, params, hander);
-    	else
-    		Utils.showLongToast(context, "请连接网络");
-    }
 
-    public static void getReq(Context context,String host, String url, RequestParams params, JsonHttpResponseHandler hander) {
-    		client.get(host + url, params, hander);
-    }
+	public static final String HOST = "http://183.129.190.82:50001/v1";
 
-    public static void getReq(Context context,String url, RequestParams params, JsonHttpResponseHandler hander) {
-        if(Utils.isNetworkAvailable(context))
-        	 client.get(HOST + url, params, hander);
-    	else
-    		Utils.showLongToast(context, "请连接网络");
-    }
-    
-    public static void downloadFile(Context context,String url,FileDownloadHandler handler){
-        client.get(url,handler);
-    }
+	// public static final String HOST = "http://10.10.105.180:9000/v1";
+	private static AsyncHttpClient client = new AsyncHttpClient();
+
+	public static void postReq(Context context, String host, String url,
+			RequestParams params, JsonHttpResponseHandler hander) {
+		client.post(host + url, params, hander);
+	}
+
+	public static void postReq(Context context, String url,
+			RequestParams params, JsonHttpResponseHandler hander) {
+		if (Utils.isNetworkAvailable(context))
+			client.post(HOST + url, params, hander);
+		else
+			Utils.showLongToast(context, "请连接网络");
+	}
+
+	public static void getReq(Context context, String host, String url,
+			RequestParams params, JsonHttpResponseHandler hander) {
+		client.get(host + url, params, hander);
+	}
+
+	public static void getReq(Context context, String url,
+			RequestParams params, JsonHttpResponseHandler hander) {
+		if (Utils.isNetworkAvailable(context))
+			client.get(HOST + url, params, hander);
+		else
+			Utils.showLongToast(context, "请连接网络");
+	}
+
+	public static void downloadFile(Context context, String url,
+			FileDownloadHandler handler) {
+		if (Utils.isNetworkAvailable(context))
+			client.get(url, handler);
+		else
+			Utils.showLongToast(context, "请连接网络");
+	}
 }
